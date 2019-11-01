c55de22350724c0475d14d6c666e05c077bb0738
Neil Zhao <neil@zzrcxb.me>
2019-10-31 23:15:06 -0500

    refactored GDB driver

--- lapi-plus/GDBDriver.py
+++ lapi-plus/GDBDriver.py
@@ -1,25 +1,22 @@
 import re
 import os
 import sys
+import git
 import json
 import shutil
 import logging
 import resource
 import datetime
+import fileinput
 import subprocess
 
 from pathlib import Path
-from multiprocessing import cpu_count, Pool, Lock, Process
 
-WORK_DIR = str(Path(__file__).parent)
 sys.path.append(str(Path(__file__).parent))
-sys.path.append('/home/neil/.virtualenvs/lapi/lib/python3.7/site-packages')
-print(sys.path)
 
 from CheckpointConvert import convert_checkpoint
 from CheckpointTemplate import MemoryMapping, RegisterValues, fill_checkpoint_template
 from Checkpoints import GDBCheckpoint
-
 
 try:
     import gdb  # pylint: disable=import-error
@@ -33,7 +30,7 @@ class CONFIGS:
     PIN_INFO_FILENAME  = '.pininfo'
     PMEM_FILENAME      = 'system.physmem.store0.pmem'
     M5CPT_FILENAME     = 'm5.cpt'
-    MMAP_FILENAME      = 'mapping.json'
+    MMAP_FILENAME      = 'mappings.json'
     COREDUMP_FILENAME  = 'pinGDB.core'
     CHECKSUM_FILENAME  = '.checksum'
     PINTOOL_PLUGIN     = 'brkpt.so'
@@ -49,71 +46,122 @@ class GDBEngine:
 
     BAD_MEM_REGIONS = ['[vvar]', '[vsyscall]']
 
-    def __init__(self,
-                 cmd:list,
-                 checkpoint_root_dir,
-                 compress_core_files,
-                 convert_checkpoints,
-                 run_dir, remote_port):
+    def __init__(self, cmd: list, ckpt_prefix: Path, run_dir: Path, compress_ckpt=True,
+                 mem_size=4*1024*1024*1024, preserve_intermediate=False):
         assert(len(cmd) > 0)
         self.binary = run_dir / cmd[0]
         self.args = cmd[1:]
+        assert(self.binary.exists())
 
-        self._pin_info = run_dir / CONFIGS.PIN_INFO_FILENAME
+        self._pin_info  = run_dir / CONFIGS.PIN_INFO_FILENAME
         self._port_info = run_dir / CONFIGS.PORT_INFO_FILENAME
-
+        # try to load port info
         if not self._port_info.exists():
             logging.error('Remote GDB port information not found, please check whether the pintool is running')
         with self._port_info.open() as f:
-            self._rgdb_port = json.load(f)['port']
+            rgdb_port = json.load(f)['port']
 
-        from lapidary.checkpoint.GDBShell import GDBShell
-        self.shell = GDBShell(self)
-        self.chk_num = 0
-        self.compress_core_files = compress_core_files
-        self.compress_processes  = {}
-        self.convert_checkpoints = convert_checkpoints
-        self.convert_processes   = {}
+        self._ckpt_prefix = ckpt_prefix
+        self._compress_ckpt = compress_ckpt
+        self.mem_size = mem_size
+        self._repo = git.Repo(search_parent_directories=True)
 
-        # Otherwise long arg strings get mutilated with '...'
-        gdb.execute('target remote :{}'.format(self._rgdb_port))
-        gdb.execute('set print elements 0')
+        if self._repo.is_dirty():
+            logging.warning('Repo is dirty! Are you developping or running final experiments?')
+
+        # Connect to remote GDB server
+        gdb.execute('target remote :{}'.format(rgdb_port))
         gdb.execute('set follow-fork-mode child')
-        # args = (gdb.execute('print $args', to_string=True)).split(' ')[2:]
-        # args = [ x.replace('"','').replace(os.linesep, '') for x in args ]
-        # assert len(args) > 0
-        # self.binary = args[0]
-        # self.args = ' '.join(args[1:])
-        if checkpoint_root_dir is not None:
-            self.chk_out_dir = Path(checkpoint_root_dir) / \
-                '{}_gdb_checkpoints'.format(Path(self.binary).name)
+
+    def run(self):
+        while self.is_running:
+            self._create_ckpt()
+            gdb.execute('c')
+
+    def _create_ckpt(self):
+        # load messages
+        with self._pin_info.open() as f:
+            pin_info = json.load(f)
+        brk_ID    = pin_info['brkID']
+        ckpt_name = 'cpt.None.SIMP-{}'.format(brk_ID)
+        pid       = pin_info['pid']
+        brk_value = pin_info['brkPoint']
+        fs_base   = pin_info['fsBase']
+        ckpt_dir  = self._ckpt_prefix / ckpt_name
+        pmem_path     = ckpt_dir / CONFIGS.PMEM_FILENAME
+        m5cpt_path    = ckpt_dir / CONFIGS.M5CPT_FILENAME
+        mmap_path     = ckpt_dir / CONFIGS.MMAP_FILENAME
+        coredump_path = ckpt_dir / CONFIGS.COREDUMP_FILENAME
+
+        # check and clear
+        if ckpt_dir.exists():
+            logging.warning('Checkpoint #{} exists, overwriting!'.format(brk_ID))
+            for file in ckpt_dir.glob('*'):  # delete everything
+                file.unlink()
         else:
-            self.chk_out_dir = Path( WORK_DIR ) / Path('{}_gdb_checkpoints'.format(Path(self.binary).name))
-        gdb.execute('set print elements 200')
+            ckpt_dir.mkdir(parents=True)
 
-        self.run_dir = run_dir
-        self.msg_info = run_dir / '.pininfo'
-        with self.msg_info.open() as f:
-            info = json.load(f)
-        self.pid = info['pid']
+        # get program states, including mmap and registers
+        mmaps, unexpanded_mmaps = self._create_mappings(pid)
+        regs = RegisterValues(fs_base)
 
+        # check if the brkpt is ready to dump
+        def ckpt_check():
+            current_pc = int(regs.get_pc_string())
+            for mapping in unexpanded_mmaps.values():
+                if current_pc in mapping and mapping.name in self.BAD_MEM_REGIONS:
+                    logging.warning('Skipping checkpoint #{} since current PC {:#} is in {}'.format(
+                                    brk_ID, current_pc, mapping.name))
+                    return False
+            return True
+        if not ckpt_check():
+            return False
 
-    def _get_virtual_addresses(self):
+        # get stack mapping
+        stack_mapping = [m for v, m in unexpanded_mmaps.items() if 'stack' in m.name]
+        assert len(stack_mapping) == 1
+        stack_mapping = stack_mapping[0]
+
+        # fill ninja2 template
+        fill_checkpoint_template(
+            m5cpt_path,
+            mappings=mmaps,
+            misc_reg_string=regs.get_misc_reg_string(),
+            int_reg_string=regs.get_int_reg_string(),
+            pc_string=regs.get_pc_string(),
+            next_pc_string=regs.get_next_pc_string(),
+            float_reg_string=regs.get_float_reg_string(),
+            mem_size=self.mem_size,
+            stack_mapping=stack_mapping,
+            brk=brk_value,
+            mmap_end=self.mmap_end,
+            timeNow=str(datetime.datetime.now(datetime.timezone.utc)),
+            repoHEAD=self._repo.head.object.hexsha)
+
+        self._dump_core_to_file(coredump_path)
+        self._dump_mappings_to_file(unexpanded_mmaps, self.mem_size, mmap_path)
+        convert_checkpoint(GDBCheckpoint(ckpt_dir, CONFIGS), True, compress=self._compress_ckpt)
+
+    def _get_virtual_addresses(self, pid):
         vaddrs  = [0]
         sizes   = [resource.getpagesize()]
         offsets = [0]
         names   = ['null']
-        p = subprocess.Popen(['gdb', self.binary, '--batch', '-ex', 'info proc mappings {}'.format(self.pid)], stdout=subprocess.PIPE)
+        p = subprocess.Popen(['gdb', self.binary, '--batch', '-ex', 'info proc mappings {}'.format(pid)], stdout=subprocess.PIPE)
         raw_mappings = p.communicate()[0].decode('utf8')
+
+        def mmap_filter(name):
+            return CONFIGS.PINTOOL_PLUGIN not in name and os.environ.get('PIN_ROOT', CONFIGS.DEFAULT_PIN_VER) not in name
 
         for entry in raw_mappings.split(os.linesep):
             matches = self.VADDR_REGEX.match(entry.strip())
             if matches:
-                if 'brpt.so' not in str(matches.group(4)).strip() and '/home/neil/pin-3.11' not in str(matches.group(4)).strip():
+                sec_name = str(matches.group(4)).strip()
+                if mmap_filter(sec_name):
                     vaddrs   += [int(matches.group(1), 16)]
                     sizes    += [int(matches.group(2), 16)]
                     offsets  += [int(matches.group(3), 16)]
-                    names    += [str(matches.group(4)).strip()]
+                    names    += [sec_name]
 
         paddrs = []
         flags = []
@@ -125,46 +173,31 @@ class GDBEngine:
 
         return paddrs, vaddrs, sizes, offsets, flags, names
 
-
-    def _create_mappings(self, filter_bad_regions=False, expand=False):
-        paddrs, vaddrs, sizes, offsets,flags, names = self._get_virtual_addresses()
+    def _create_mappings(self, pid):
+        paddrs, vaddrs, sizes, offsets,flags, names = self._get_virtual_addresses(pid)
         assert len(paddrs) == len(vaddrs)
         assert len(paddrs) == len(sizes)
         assert len(paddrs) == len(flags)
         assert len(paddrs) == len(names)
         mappings = {}
+        unexpanded = {}
         index = 0
         pgsize = resource.getpagesize()
         for p, v, s, o, f, name in zip(paddrs, vaddrs, sizes, offsets, flags, names):
-            if expand:
-                for off in range(0, s, pgsize):
-                    paddr = p + off if p != 0 else 0
-                    vaddr = v + off
-                    offset = o + off
-                    mappings[vaddr] = MemoryMapping(
-                        index, paddr, vaddr, pgsize, offset, f, name)
-            else:
-                mappings[v] = MemoryMapping(index, p, v, s, o, f, name)
+            unexpanded[v] = MemoryMapping(index, p, v, s, o, f, name)
+            for off in range(0, s, pgsize):
+                paddr = p + off if p != 0 else 0
+                vaddr = v + off
+                offset = o + off
+                mappings[vaddr] = MemoryMapping(
+                    index, paddr, vaddr, pgsize, offset, f, name)
             index += 1
-        return mappings
-
-
-    @staticmethod
-    def _create_convert_process(checkpoint_dir):
-        gdb_checkpoint = GDBCheckpoint(checkpoint_dir)
-        proc = Process(target=convert_checkpoint,
-                       args=(gdb_checkpoint, True))
-        proc.start()
-        return proc
+        return mappings, unexpanded
 
     def _dump_core_to_file(self, file_path):
         gdb.execute('set use-coredump-filter off')
         gdb.execute('set dump-excluded-mappings off')
         gdb.execute('gcore {}'.format(str(file_path)))
-        if self.convert_checkpoints:
-            print('Creating convert process for {}'.format(str(file_path.parent)))
-            convert_proc = GDBEngine._create_convert_process(file_path.parent)
-            self.convert_processes[file_path.parent] = convert_proc
 
     def _dump_mappings_to_file(self, mappings, mem_size, file_path):
         json_mappings = {'mem_size': mem_size}
@@ -174,134 +207,75 @@ class GDBEngine:
         with file_path.open('w') as f:
             json.dump(json_mappings, f, indent=4)
 
+    @property
+    def is_running(self):
+        # TODO: test on multi-threaded programs
+        if not any(gdb.selected_inferior().threads()):
+            logging.info('Program stopped')
+            return False
+        else:
+            return True
 
-    def _calculate_memory_size(self, mappings):
-        # 4GB
-        return 4294967296
-
-    def get_mmap_end(self):
+    @property
+    def mmap_end(self):
         return 18446744073692774400
 
-    def _create_gem5_checkpoint(self, debug_mode):
-        with self._pin_info.open() as f:
-            pin_info = json.load(f)
-        brk_ID    = pin_info['brkID']
-        ckpt_name = 'cpt.None.SIMP-{}'.format(brk_ID)
-        pid       = pin_info['pid']
-        brk_value = pin_info['brkPoint']
-        fs_base   = pin_info['fsBase']
-        ckpt_dir  = self.chk_out_dir / ckpt_name
-        pmem_path     = ckpt_dir / CONFIGS.PMEM_FILENAME
-        m5cpt_path    = ckpt_dir / CONFIGS.M5CPT_FILENAME
-        mmap_path     = ckpt_dir / CONFIGS.MMAP_FILENAME
-        coredump_path = ckpt_dir / CONFIGS.COREDUMP_FILENAME
 
+class LogFormatter(logging.Formatter):
+    def __init__(self, style='{'):
+        logging.Formatter.__init__(self, style=style)
 
-        chk_loc = ckpt_dir
-        if chk_loc.exists():
-            print('Warning: {} already exists, overriding.'.format(str(chk_loc)))
-        else:
-            chk_loc.mkdir(parents=True)
-        pmem_name = 'system.physmem.store0.pmem'
-        chk_file = 'm5.cpt'
+    def format(self, record):
+        from colorama import Fore
+        stdout_template = '{levelname}' + Fore.RESET + '] {threadName}: ' + '{message}'
+        stdout_head = '[%s'
 
-        template_mappings = self._create_mappings(True, expand=True)
-        regs = RegisterValues(self.fs_base)
+        all_formats = {
+          logging.DEBUG: logging.StrFormatStyle(stdout_head % Fore.LIGHTBLUE_EX + stdout_template),
+          logging.INFO: logging.StrFormatStyle(stdout_head % Fore.GREEN + stdout_template),
+          logging.WARNING: logging.StrFormatStyle(stdout_head % Fore.LIGHTYELLOW_EX + stdout_template),
+          logging.ERROR: logging.StrFormatStyle(stdout_head % Fore.LIGHTRED_EX + stdout_template),
+          logging.CRITICAL: logging.StrFormatStyle(stdout_head % Fore.RED + stdout_template)
+        }
 
-        total_mem_size = 4294967296
+        self._style = all_formats.get(record.levelno, logging.StrFormatStyle(logging._STYLES['{'][1]))
+        self._fmt = self._style._fmt
+        result = logging.Formatter.format(self, record)
+        return result
 
-        file_mappings = self._create_mappings(True)
+    @classmethod
+    def init_logger(cls, level=logging.INFO, file_prefix: str=None):
+        if file_prefix and not os.path.exists('log'):
+            os.mkdir('log')
 
-        stack_mapping = [ m for v, m in file_mappings.items() if 'stack' in m.name ]
-        assert len(stack_mapping) == 1
-        stack_mapping = stack_mapping[0]
+        root_logger = logging.getLogger()
+        root_logger.setLevel(level)
 
-        brk_value = self._get_brk_value(self._pin_info)
-        mmap_end_value = self.get_mmap_end()
+        log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(threadName)s: %(message)s")
 
-        fill_checkpoint_template(
-            output_file=str(chk_loc / chk_file),
-            mappings=template_mappings,
-            misc_reg_string=regs.get_misc_reg_string(),
-            int_reg_string=regs.get_int_reg_string(),
-            pc_string=regs.get_pc_string(),
-            next_pc_string=regs.get_next_pc_string(),
-            float_reg_string=regs.get_float_reg_string(),
-            mem_size=total_mem_size,
-            stack_mapping=stack_mapping,
-            brk=brk_value,
-            mmap_end=mmap_end_value,
-            timeNow=str(datetime.datetime.now(datetime.timezone.utc)))
+        if file_prefix:
+            if not os.path.exists(file_prefix):
+                os.mkdir(file_prefix)
+            file_handler = logging.FileHandler(os.path.join('log', str(datetime.datetime.now()).replace(':', '_') + '.log'))
+            file_handler.setFormatter(log_formatter)
+            root_logger.addHandler(file_handler)
 
-        self._dump_core_to_file(chk_loc / 'gdb.core')
-        self._dump_mappings_to_file(file_mappings, total_mem_size,
-            chk_loc / 'mappings.json')
-        self.chk_num += 1
-
-    def _can_create_valid_checkpoint(self):
-        mappings = self._create_mappings()
-        regs = RegisterValues(self.fs_base)
-
-        current_pc = int(regs.get_pc_string())
-        for vaddr, mapping in mappings.items():
-            if current_pc in mapping and mapping.name in GDBEngine.BAD_MEM_REGIONS:
-                print('Skipping checkpoint at {} since it is in {} region'.format(
-                      hex(current_pc), mapping.name))
-                return False
-
-        return True
-
-    @staticmethod
-    def _get_brk_value(pin_info):
-        with pin_info.open() as f:
-            return json.load(f)['brkPoint']
-
-    def _run_base(self, pin_info):
-        with pin_info.open() as f:
-            return json.load(f)['fsBase']
-
-    def _poll_background_processes(self, wait=False):
-        timeout = 0.001
-        if wait:
-            print('Waiting for background processes to complete before exit.')
-            timeout = None
-        gzip_complete = []
-        for file_path, gzip_proc in self.compress_processes.items():
-            gzip_proc.join(timeout)
-            if not gzip_proc.is_alive():
-                gzip_complete += [file_path]
-                if self.convert_checkpoints:
-                    print('Creating convert process for {} after gzip'.format(
-                        str(file_path)))
-                    convert_proc = GDBEngine._create_convert_process(file_path)
-                    self.convert_processes[file_path.parent] = convert_proc
-        for key in gzip_complete:
-            print('Background gzip for {} completed'.format(key))
-            self.compress_processes.pop(key)
-
-        convert_complete = []
-        for file_path, convert_proc in self.convert_processes.items():
-            convert_proc.join(timeout)
-            if not convert_proc.is_alive():
-                convert_complete += [file_path]
-        for key in convert_complete:
-            print('Background convert for {} completed'.format(key))
-            self.convert_processes.pop(key)
-
-    def try_create_checkpoint(self, debug_mode=False):
-        # self._poll_background_processes()
-
-        if self._can_create_valid_checkpoint():
-            print('Creating checkpoint #{}'.format(self.chk_num))
-            self._create_gem5_checkpoint(debug_mode)
+        console_handler = logging.StreamHandler(stream=sys.stderr)
+        console_handler.setFormatter(cls())
+        root_logger.addHandler(console_handler)
 
 
 if __name__ == "__main__":
-    run_dir = Path('/home/neil/Simpoint3.2/bin')
-    ckpt_dir = Path('/home/neil/test/libquantum')
+    LogFormatter.init_logger()
+    lines = []
+    with fileinput.input() as fd:
+        for line in fd:
+            lines.append(line)
+        options = json.loads('\n'.join(lines))
 
-    with open(run_dir / '.portinfo') as f:
-        gdb_port = json.load(f)['port']
-    engine = GDBEngine(['./libquantum', '1397', '8'], '/home/neil/test', False, True, run_dir, gdb_port)
-    engine.fs_base = engine._run_base(run_dir / CONFIGS.PIN_INFO_FILENAME)
-    engine.try_create_checkpoint()
+    cmd = options['cmd']
+    ckpt_prefix = options['ckpt-prefix']
+    run_dir = options['run-dir']
+
+    engine = GDBEngine(cmd, Path(ckpt_prefix), Path(run_dir))
+    engine.run()
